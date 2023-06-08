0fb12fceb7341560bff26f410d076c85e504d968
splendorgg <splendorg@protonmail.com>
2023-06-08 20:59:11 +0300

    second

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/splendorgg/learning-go
+
+go 1.19
---
+++ test01.go
@@ -0,0 +1,9 @@
+package test
+
+func Testing() string {
+	return "This is a test1"
+}
+
+func Testing2() string {
+	return "This is a test2"
+}
